4dcbf718990ac445d138b50bde45e8670e31d5a2
--global <--global>
2025-08-06 14:37:17 +0800

    添加h/j/k/l移动支持

--- main.c
+++ main.c
@@ -22,18 +22,21 @@ int main() {
         int ch = getch();
         switch (ch) {
             case KEY_UP:
+            case 'k':
                 cursor_move_up(&cursor);
                 break;
             case KEY_DOWN:
+            case 'j':
                 cursor_move_down(&cursor);
                 break;
             case KEY_LEFT:
+            case 'h':
                 cursor_move_left(&cursor);
                 break;
             case KEY_RIGHT:
+            case 'l':
                 cursor_move_right(&cursor);
                 break;
-            case 'd':
             case ' ':
                 cursor_draw(&cursor);
                 break;
